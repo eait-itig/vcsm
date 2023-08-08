788777c093f7bb03de448436ac74c9ebe37e3d9f
Alex Wilson <alex@uq.edu.au>
2023-08-08 11:12:27 +1000

    initial commit

---
+++ src/vcsm/agentext.py
@@ -0,0 +1,156 @@
+#
+# Copyright 2023 The University of Queensland
+# Author: Alex Wilson <alex@uq.edu.au>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+"""
+Agent extension and key wrapper class
+"""
+
+import paramiko.agent
+from paramiko import Message
+
+from ecdsa.keys import VerifyingKey, BadSignatureError
+from ecdsa.ellipticcurve import Point
+from ecdsa.der import UnexpectedDER
+from ecdsa.util import sigencode_der, sigdecode_der
+
+from tlslite.utils.ecdsakey import ECDSAKey
+from tlslite.utils.ecc import decodeX962Point, getCurveByName
+from tlslite.utils.cryptomath import secureHash
+from tlslite.utils.compat import compatHMAC
+
+from tlslite import X509
+
+SSH_AGENT_SUCCESS = 6
+SSH_AGENTC_EXTENSION = bytes([27])
+
+class Agent(paramiko.agent.Agent):
+    def query_extensions(self):
+        msg = Message()
+        msg.add_byte(SSH_AGENTC_EXTENSION)
+        msg.add_string("query")
+        ptype, result = self._send_message(msg)
+        if ptype != SSH_AGENT_SUCCESS:
+            return []
+        n = result.get_int()
+        r = []
+        for x in range(0, n):
+            r.append(result.get_text())
+        return r
+
+    def has_extension(self, ext):
+        exts = self.query_extensions()
+        if ext in exts:
+            return True
+        return False
+
+class AgentKeyExt(paramiko.agent.AgentKey):
+    def sign_prehash_data(self, data):
+        msg = Message()
+        msg.add_byte(SSH_AGENTC_EXTENSION)
+        msg.add_string('sign-prehash@arekinath.github.io')
+        msg.add_string(self.blob)
+        msg.add_string(data)
+        msg.add_int(0)
+        ptype, result = self.agent._send_message(msg)
+        if ptype != SSH_AGENT_SUCCESS:
+            raise Exception('ssh-agent failed to sign prehashed data')
+        return result.get_binary()
+
+    def get_x509_cert(self):
+        msg = Message()
+        msg.add_byte(SSH_AGENTC_EXTENSION)
+        msg.add_string('x509-certs@joyent.com')
+        msg.add_string(self.blob)
+        msg.add_int(0)
+        ptype, result = self.agent._send_message(msg)
+        if ptype != SSH_AGENT_SUCCESS:
+            raise Exception('ssh-agent failed to return x509 certs')
+        return result.get_binary()
+paramiko.agent.AgentKey = AgentKeyExt
+
+class SSHAgentECDSAKey(ECDSAKey):
+    def __init__(self, fp):
+        self._agent = Agent()
+        ks = [x for x in self._agent.get_keys() if x.get_fingerprint() == fp]
+        if len(ks) < 1:
+            raise Exception('Key fingerprint not found in agent')
+        if not self._agent.has_extension('sign-prehash@arekinath.github.io'):
+            raise Exception('Agent lacks the prehash sign extension')
+        self._key = ks[0]
+        tname = None
+        if self._key.get_name() == 'ecdsa-sha2-nistp256':
+            tname = 'secp256r1'
+        elif self._key.get_name() == 'ecdsa-sha2-nistp384':
+            tname = 'secp384r1'
+        elif self._key.get_name() == 'ecdsa-sha2-nistp521':
+            tname = 'secp521r1'
+        else:
+            raise Exception('Unsupported EC alg')
+        self._curve = getCurveByName(tname)
+        msg = Message(self._key.asbytes())
+        msg.get_text()  # alg
+        msg.get_text()  # curve
+        self._point = decodeX962Point(msg.get_binary(), self._curve)
+        self.public_key = VerifyingKey.from_public_point(self._point, self._curve)
+        self.private_key = self.public_key
+
+    def key_type(self):
+        return 'ecdsa'
+
+    def __len__(self):
+        if self._key.get_name() == 'ecdsa-sha2-nistp256':
+            return 256
+        elif self._key.get_name() == 'ecdsa-sha2-nistp384':
+            return 384
+        elif self._key.get_name() == 'ecdsa-sha2-nistp521':
+            return 521
+
+    def hasPrivateKey(self):
+        return True
+
+    def get_cert(self):
+        der = self._key.get_x509_cert()
+        x = X509()
+        x.parseBinary(der)
+        return x
+
+    @staticmethod
+    def generate(bits):
+        raise NotImplementedError()
+
+    def acceptsPassword(self):
+        return False
+
+    def _sign(self, bytes, rsaScheme=None, hAlg='sha256', sLen=None):
+        return self._key.sign_prehash_data(bytes)
+
+    def _verify(self, signature, hash_bytes):
+        try:
+            return self.public_key.verify_digest(compatHMAC(signature),
+                                                 compatHMAC(hash_bytes),
+                                                 sigdecode_der)
+        # https://github.com/warner/python-ecdsa/issues/114
+        except (BadSignatureError, UnexpectedDER, IndexError, AssertionError):
+            return False
---
+++ src/vcsm/getsi.py
@@ -0,0 +1,158 @@
+#
+# Copyright 2023 The University of Queensland
+# Author: Alex Wilson <alex@uq.edu.au>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+"""
+Implementation of getServiceInstance
+"""
+
+from .agentext import SSHAgentECDSAKey, Agent
+from tlslite import TLSConnection, HTTPTLSConnection, X509, X509CertChain, parsePEMKey
+from xml.etree import ElementTree
+
+from http.client import HTTPSConnection
+from urllib.parse import urlencode, urlparse, parse_qs
+
+from http.cookies import SimpleCookie
+
+from certvalidator import CertificateValidator, ValidationContext
+from certvalidator.errors import PathValidationError
+from asn1crypto.x509 import Certificate
+
+from pyVmomi import vim, SoapStubAdapter
+
+import json
+import logging
+
+def getServiceInstance(host: str, upn: str):
+    log = logging.getLogger('vcsm')
+
+    vctx = ValidationContext(
+        allow_fetching = True,
+        revocation_mode = 'hard-fail')
+
+    agent = Agent()
+    key = None
+    log.debug('searching for key')
+    for k in agent.get_keys():
+        log.debug(f"looking at key {k.get_fingerprint().hex()}")
+        try:
+            der = k.get_x509_cert()
+            cert = Certificate.load(der)
+            val = CertificateValidator(cert, validation_context = vctx)
+            val.validate_usage(set(['digital_signature']))
+            if not cert.subject_alt_name_value:
+                continue
+            for san in cert.subject_alt_name_value:
+                if san.name != 'other_name':
+                    continue
+                try:
+                    oid = san.native['type_id']
+                    v = san.native['value']
+                except:
+                    continue
+                if oid == '1.3.6.1.4.1.311.20.2.3':
+                    log.debug(f"key {k.get_fingerprint().hex()} has upn {v}")
+                    if v == upn:
+                        key = k
+                        break
+            if key is not None:
+                break
+        except Exception as e:
+            log.debug(f"failed to check upn: {e}")
+            continue
+    if key is None:
+        raise Exception(f"Failed to find any agent key with UPN {upn}")
+    fp = key.get_fingerprint()
+    log.debug(f"using key {fp.hex()}")
+
+    log.debug('getting SAMLRequest from /ui/login')
+    conn = HTTPSConnection(host = host, port = 443)
+    conn.request('GET', '/ui/login')
+    r = conn.getresponse()
+    if r.status != 302:
+        raise Exception(f"Expected 302 from /ui/login, got a {r.status}")
+    uri = urlparse(r.getheader('Location'))
+    r.read()
+
+    log.debug('starting smartcard auth')
+    pkey = SSHAgentECDSAKey(fp)
+
+    x509 = pkey.get_cert()
+    chain = X509CertChain([x509])
+
+    smconn = HTTPTLSConnection(
+        host = host,
+        port = 3128,
+        certChain = chain,
+        privateKey = pkey)
+    smconn.request('POST', '/websso/SAML2/SSOCAC/vsphere.local?' + uri.query,
+        body = urlencode({'CastleAuthorization': 'TLSClient Og=='}),
+        headers = {'content-type': 'application/x-www-form-urlencoded'})
+    r = smconn.getresponse()
+    if r.status != 200:
+        raise Exception(f"Expected 200 from ssocac, got a {r.status}")
+    body = r.read()
+
+    root = ElementTree.fromstring(body.decode('utf-8'))
+    body = root.find('body')
+    form = body.find('form')
+    inputs = form.findall('input')
+    post = {}
+    for inp in inputs:
+        if inp.get('name') is not None:
+            post[inp.get('name')] = inp.get('value')
+
+    log.debug("converting back to /ui cookies...")
+    conn.request('POST', '/ui/saml/websso/sso',
+        body = urlencode(post),
+        headers = {'content-type': 'application/x-www-form-urlencoded'})
+    r = conn.getresponse()
+    r.read()
+    if r.status != 302:
+        raise Exception(f"Expected 302 from /ui/saml/websso/sso, got a {r.status}")
+
+    jar = SimpleCookie()
+    for k,v in r.getheaders():
+        if k == 'set-cookie':
+            jar.load(v)
+    cookies = jar.output(attrs=[], header='', sep=';')
+
+    log.debug("getting session info...")
+    conn.request('GET', '/ui/usersession/serverInfo',
+        headers = {'cookie': cookies})
+    r = conn.getresponse()
+    data = json.loads(r.read())
+
+    sinfos = data['serversInfo']
+    my_sinfos = [x for x in sinfos if x['name'] == host]
+    soap_cookie = json.loads(my_sinfos[0]['sessionCookie'])
+
+    log.debug('creating service instance...')
+
+    soap = SoapStubAdapter(host = host)
+    soap.cookie = 'vmware_soap_session=' + soap_cookie
+
+    si = vim.ServiceInstance('ServiceInstance', soap)
+    return si
